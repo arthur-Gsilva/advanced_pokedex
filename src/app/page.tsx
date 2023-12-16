f37dbcd4e41f9acbd09f59a7234f55fa7c6121cd
Arthur Gabriel <arthurgdasilva77@gmail.com>
2023-12-16 03:12:19 -0300

    redirection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
+"use client"
+
+
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
+
 const Page = () => {
+
+    const router = useRouter()
+
+    useEffect(() => {
+        router.push('/pokemon/bulbasaur')
+    }, [])
+
     return(
         <div>
-           h1
-           title
+          
         </div>
     )
 }
